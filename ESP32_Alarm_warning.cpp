c25b45938af2b72fc035dbeb9c63c700ff7bfd6c
Brian Correa Casas <brian2010correa@hotmail.com>
2025-04-01 08:47:59 -0300

    Update ESP32_Alarm_warning.cpp

--- ESP32_Alarm_warning.cpp
+++ ESP32_Alarm_warning.cpp
@@ -16,31 +16,30 @@ Adafruit_ST7789 tft = Adafruit_ST7789(LCD_CS, LCD_DC, LCD_RST);
 
 // Configuración WiFi
 const char *ssid = "TU_SSID";
-const char *password = "TU_CONTRASEÑA_WIFI";
+const char *password = "TU_PASSWORD";
 
 // Configuración del correo
 ESP_Mail_Session session;
 SMTPSession smtp;
 SMTP_Message message;
 
-const char *email_sender = "TU_EMAIL@gmail.com";  // Cambiar a tu correo electrónico
-const char *email_password = "TU_CONTRASEÑA_APP"; // Contraseña de aplicación, obtener desde la configuración de seguridad de tu cuenta
+const char *email_sender = "TU_CORREO";
+const char *email_password = "TU_CONTRASEÑA";
 const char *smtp_server = "smtp.gmail.com";
 const int smtp_port = 465;
-
-const char *email_recipient = "DESTINATARIO_EMAIL"; // Correo del destinatario
+const char *email_recipient = "DESTINATARIO_CORREO";
 
 // Configuración del pin GPIO
-const int alarmaPin = 13;
+const int alarmaPin = 10;
 bool alarmaActiva = false;
-bool estadoAnterior = LOW; // Guarda el último estado leído
+bool estadoAnterior = LOW;
 unsigned long ultimaVezCorreo = 0;
-const unsigned long tiempoEspera = 300000; // 5 minutos (300000 ms)
+const unsigned long tiempoEspera = 300000;
 
 // Configuración NTP
 const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = -10800;   // UTC-3 (ajustar según tu zona horaria)
-const int daylightOffset_sec = 3600; // Cambio horario, ajusta si es necesario
+const long gmtOffset_sec = -10800;
+const int daylightOffset_sec = 3600;
 
 void configurarHora()
 {
@@ -57,8 +56,6 @@ void configurarHora()
 void setup()
 {
     Serial.begin(115200);
-
-    // Inicializar la pantalla
     tft.init(135, 240);
     tft.setRotation(1);
     tft.fillScreen(ST77XX_BLACK);
@@ -66,20 +63,16 @@ void setup()
     tft.setTextSize(2.5);
     tft.println("Iniciando...");
 
-    // Configurar el tiempo
     configurarHora();
-
-    // Configurar el pin como entrada con pull-down para evitar lecturas erróneas
     pinMode(alarmaPin, INPUT_PULLDOWN);
 
-    // Conectar a WiFi
     mostrarEnPantalla("Conectando a WiFi...");
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
-        mostrarEnPantalla("Conectando...");
+        mostrarEnPantalla(".");
     }
     mostrarEnPantalla("\nWiFi conectado.");
     Serial.println("\nConexión WiFi establecida.");
@@ -89,7 +82,6 @@ void loop()
 {
     int estadoActual = digitalRead(alarmaPin);
 
-    // Detectar el cambio de estado de LOW -> HIGH (activación)
     if (estadoActual == HIGH && estadoAnterior == LOW)
     {
         if (!alarmaActiva)
@@ -97,55 +89,46 @@ void loop()
             alarmaActiva = true;
             ultimaVezCorreo = millis();
             enviarCorreo();
-            mostrarEnPantalla("¡Alarma activada!");
-            Serial.println("¡Alarma activada!");
+            mostrarEnPantalla("\n\n\n\n\n\n\n\nALERTA\nALARMA ACTIVADA");
+            Serial.println("\nALARMA ACTIVADA");
         }
     }
-    // Detectar el cambio de estado de HIGH -> LOW (desactivación)
     else if (estadoActual == LOW && estadoAnterior == HIGH)
     {
         alarmaActiva = false;
         mostrarEnPantalla("Alarma inactiva...");
         Serial.println("Alarma inactiva...");
     }
-
-    estadoAnterior = estadoActual; // Actualizar el estado anterior
+    estadoAnterior = estadoActual;
 }
 
 void enviarCorreo()
 {
     Serial.println("Enviando correo...");
+    smtp.debug(1);
 
-    smtp.debug(1); // Activa la depuración para ver errores
-
-    // Configurar sesión SMTP
     session.server.host_name = smtp_server;
     session.server.port = smtp_port;
     session.login.email = email_sender;
     session.login.password = email_password;
     session.login.user_domain = "";
 
-    // Limpiar destinatarios antes de agregar uno nuevo
     message.clearRecipients();
-
-    // Configurar los datos del mensaje
-    message.sender.name = "Alarma Incendios";
+    message.sender.name = "Alarma";
     message.sender.email = email_sender;
     message.subject = "¡Alarma activada!";
     message.addRecipient("Contacto", email_recipient);
-    message.text.content = "La alarma de incendios ha sido activada. Por favor, verifica la situación.";
+    message.text.content = "Se ha activado la alarma. Verifique la situación.";
     message.text.charSet = "utf-8";
     message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
     message.priority = esp_mail_smtp_priority_high;
 
-    // Conectar al servidor SMTP
     if (!smtp.connect(&session))
     {
         Serial.println("Error al conectar con el servidor SMTP: " + smtp.errorReason());
         return;
     }
 
-    // Enviar el correo
     if (!MailClient.sendMail(&smtp, &message))
     {
         Serial.println("Error al enviar el correo: " + smtp.errorReason());
@@ -156,17 +139,9 @@ void enviarCorreo()
     }
 }
 
-// Función para mostrar mensajes en la pantalla
 void mostrarEnPantalla(String mensaje)
 {
     tft.fillScreen(ST77XX_BLACK);
-    tft.setCursor(10, 10);
-    tft.println(mensaje);
-}
-
-void mostrarEnPantallaAlerta(String mensaje)
-{
-    tft.fillScreen(ST77XX_WHITE);
     tft.setCursor(10, 10);
     tft.println(mensaje);
 }
